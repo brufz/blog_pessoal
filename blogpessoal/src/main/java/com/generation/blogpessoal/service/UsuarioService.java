b086f1f932c1daeccf5cb9da5f5a5197c162f152
Bruna Franchi <bfzdev@gmail.com>
2022-07-14 16:45:10 -0300

    adicionando código de JUnit

--- blogpessoal/src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.UserLogin;
 import com.generation.blogpessoal.model.Usuario;
@@ -14,37 +16,63 @@ import com.generation.blogpessoal.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	public UsuarioRepository usuarioRepository;
-	
-	public Usuario cadastrarUsuario(Usuario usuario) {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		
-		String senhaEncoder = encoder.encode(usuario.getSenha());
-		usuario.setSenha(senhaEncoder);
 
-		return usuarioRepository.save(usuario);
+	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
+		if (usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent()) {
+			return Optional.empty();
+		} else {
+			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+		}
+		return Optional.of(usuarioRepository.save(usuario));
+	}
+
+	public Optional<Usuario> atualizarUsuario(Usuario usuario){
+		if(usuarioRepository.findById(usuario.getId()).isPresent()) {
+			Optional<Usuario> buscaUsuario = usuarioRepository.findByUsuario(usuario.getUsuario());
+			
+			if((buscaUsuario.isPresent()) && (buscaUsuario.get().getId() != usuario.getId()))
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário ja existe", null);
+			
+			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+			
+			return Optional.ofNullable(usuarioRepository.save(usuario));
+		}
+		return Optional.empty();
 	}
 	
-	public Optional<UserLogin> logar(Optional <UserLogin> user){
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		Optional <Usuario> usuario = usuarioRepository.findByUsuario(user.get().getUsuario());
 		
-		if(usuario.isPresent()) {
+	public Optional<UserLogin> logar(Optional<UserLogin> user) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = usuarioRepository.findByUsuario(user.get().getUsuario());
+
+		if (usuario.isPresent()) {
 			if (encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
 				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
 				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
 				String authHeader = "Basic " + new String(encodedAuth);
-				
+
 				user.get().setToken(authHeader);
 				user.get().setNome(usuario.get().getNome());
 
 				return user;
 			}
 		}
-		
+
 		return null;
+
+	}
+	
+	
+
+	private String criptografarSenha(String senha) {
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		return encoder.encode(senha);
+
 	}
 
 }
