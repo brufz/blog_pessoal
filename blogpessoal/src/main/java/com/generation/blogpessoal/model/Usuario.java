a33bed6160e88237df80aedba97186912539b3d2
Bruna Franchi <bfzdev@gmail.com>
2022-07-15 15:28:20 -0300

    deploy do projeto blog pessoal

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -5,8 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Entity
 @Table (name = "tb_usuario")
@@ -21,7 +24,8 @@ public class Usuario {
 	private String nome;
 	
 	@NotNull
-	@Size(min = 5, max = 100)
+	@Email(message = "O atributo usuário deve ser um e-mail valido")
+	@Schema(example = "email@email.com.br")
 	private String usuario;
 	
 	@NotNull
